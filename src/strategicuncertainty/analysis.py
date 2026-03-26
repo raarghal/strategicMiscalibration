18e49f56b3a03c0556632804d4a464ee129f998a
raarghal <rarghal@seas.upenn.edu>
2026-03-26 14:45:05 -0400

    analysis logic for user decision

--- src/strategicuncertainty/analysis.py
+++ src/strategicuncertainty/analysis.py
@@ -165,7 +165,8 @@ def load_and_prepare_dataframe(datafile: Path) -> pd.DataFrame:
     df = pd.read_csv(datafile)
     # If needed in future:
     # df["round"] = df["round"] + 1
-    df["Delegated"] = df["agent_payoff"] / max(df["agent_payoff"])
+    delegation_map = {"DELEGATE": 1, "SELF_COMPLETE": 0}
+    df["Delegated"] = [delegation_map[decision] for decision in df["user_decision"]]
     return df.round(4)
 
 
